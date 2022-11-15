8dbaa56a191c5211221be9b375f99e6b0caef03b
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2022-11-15 11:20:55 +0100

    Lol

--- src/components/HeroCard/HeroStats.vue
+++ src/components/HeroCard/HeroStats.vue
@@ -26,9 +26,11 @@ padding-left: 15px;
 padding-right: 15px;
 margin-bottom: 30px;
 text-align: center;
-font-size: 17px;
+font-size: 14px;
 color: black;
 font-weight: bold;
+border-top-right-radius: 7px;
+border-bottom-right-radius: 7px;
 }
 h1{
   font-size: 21px;
@@ -39,10 +41,21 @@ h1{
 }
 
 .bad{
-  background-color: red;
+  background-color: linear-gradient(111.4deg, rgb(246, 4, 26) 0.4%, rgb(251, 139, 34) 100.2%);
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
+
 }
 
 .good{
   background: linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
+}
+
+.neutral{
+  background-color: linear-gradient(111.4deg, rgb(51, 139, 147) 0.4%, rgb(251, 139, 34) 100.2%);
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
 }
 </style>
--- src/components/HeroCard/StatField.vue
+++ src/components/HeroCard/StatField.vue
@@ -1,20 +1,41 @@
 <script  setup>
 const props = defineProps(['skill', 'value'])
+
+const icon = computed (() => {
+  switch (props.skill) {
+    case 'intelligence':
+      return 'i-game-icons:brain'
+    case 'strength':
+      return 'i-game-icons:muscle-up'
+    case 'speed':
+      return 'i-game-icons-running-ninja'
+    case 'durability':
+      return 'i-game-icons-crenulated-shield'
+    case 'power':
+      return 'i-game-icons-fist'
+    case 'combat':
+      return 'i-game-icons-crossed-swords'
+  }
+})
 </script>
 
 <template>
-  <div flex justify-between>
-    <span>{{ skill }}</span>
+  <div flex justify-between class="stats">
+    <span> <div :class="icon" inline-block class="icons" />{{ '\xa0' }}{{ skill[0].toUpperCase() + skill.substring(1, skill.length) }}</span>
     <span>{{ value }}</span>
   </div>
 </template>
 
 <style>
-div{
-  border-bottom-right-radius: 7px;
-  border-top-right-radius: 7px;
-  display: block;
+.stats{
+  vertical-align: middle;
+  border: 2px solid white;
+  margin-bottom: 9px;
     color: white;
     font-family: "DM Sans",ui-sans-serif;
     }
+
+.icons{
+  vertical-align: middle;
+  }
 </style>
